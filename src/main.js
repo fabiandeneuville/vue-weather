6bec17ce400787451c1e33387d2e512f4f4f264b
Deneuville Fabian <deneuville.fabian@live.fr>
2022-07-31 00:22:32 +0200

    Installing and setting up FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* Importing fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* Importing fontawesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* Importing specific icons to be used on the project */
+import { faInfo } from '@fortawesome/free-solid-svg-icons';
+
+/* Adding icons to the library */
+library.add(faInfo);
+
+/* Adding font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false
 
 new Vue({
