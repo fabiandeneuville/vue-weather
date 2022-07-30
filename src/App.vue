746aad9037a65f7447c74689be0cf459bd1d2f8f
Deneuville Fabian <deneuville.fabian@live.fr>
2022-07-31 00:03:18 +0200

    Inporting main.scss file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <headerBlock></headerBlock>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import headerBlock from './components/header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      'headerBlock': headerBlock
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
+  @import './style/main.scss';
+
 </style>
