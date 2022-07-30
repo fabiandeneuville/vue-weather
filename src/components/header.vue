5e5404f5fdb1e2d6c28ae8208cf5f93a033358b4
Deneuville Fabian <deneuville.fabian@live.fr>
2022-07-31 00:03:47 +0200

    Working on header component

---
+++ src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+    
+    <div class="header">
+
+        <h1 class="header__heading">Vue Weather</h1>
+
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'headerBlock',
+    data(){
+        return {
+            
+        }
+    }
+}
+
+</script>
