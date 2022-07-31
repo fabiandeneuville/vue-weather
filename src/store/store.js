fb5f3f4b0fa225b96101bc0ab7e9650123fcb0bc
Deneuville Fabian <deneuville.fabian@live.fr>
2022-07-31 18:56:52 +0200

    Adding vuex and store to the project

--- src/store/store.js
+++ src/store/store.js
@@ -4,16 +4,40 @@ import Vuex from 'vuex'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state : {        
+    state : {     
         'location' : undefined,
+        'country' : undefined,
         'description' : undefined,
-        'temperature' : undefined
+        'temperature' : undefined,
+        'humidity' : undefined,
+        'sunrise' : undefined,
+        'sunset' : undefined,
     },
     getters : {
 
     },
     mutations : {
-
+        setLocation(state, location){
+            state.location = location
+        },
+        setCountry(state, country){
+            state.country = country
+        },
+        setDescription(state, description){
+            state.description = description
+        },
+        setTemperature(state, temperature){
+            state.temperature = temperature
+        },
+        setHumidity(state, humidity){
+            state.humidity = humidity
+        },
+        setSunrise(state, sunrise){
+            state.sunrise = sunrise
+        },
+        setSunset(state, sunset){
+            state.sunset = sunset
+        }
     },
     actions : {
 
