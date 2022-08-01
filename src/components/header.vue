b72a495deb1ea66261040926b0c411d52b5ab0bf
Deneuville Fabian <deneuville.fabian@live.fr>
2022-08-01 13:26:49 +0200

    Changing header style

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,10 +2,6 @@
     
     <div class="header">
 
-        <button class="header__info-button">
-            <font-awesome-icon icon="fa-solid fa-info" />
-        </button>
-
         <h1 class="header__heading">Vue Weather</h1>
 
     </div>
