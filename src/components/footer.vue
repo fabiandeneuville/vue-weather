e7ad15ec4f9541ca272f118eb6914e47ad35c589
Deneuville Fabian <deneuville.fabian@live.fr>
2022-08-02 01:20:58 +0200

    Adding API info in the footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,6 +3,7 @@
     <div>
         <footer class="footer">
             <p class="footer__text">Les données météo proviennent de l'API <a v-bind:href="apiUrl" target="blank">OpenWeather</a></p>
+            <p class="footer__text">L'utilisation de l'application est limités à 1000 recherches par mois.</p>
             <p class="footer__text">Fabian Deneuville - {{ currentYear }}</p>
         </footer>
     </div>
